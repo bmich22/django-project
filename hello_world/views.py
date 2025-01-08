be47d39dbe05ce8cee1af32e162cbf8c6114e27f
Barbara Michel <barbmichel22@gmail.com>
2025-01-08 13:40:28 +0000

    Modify the index function in the hello_world/views.py to evaluate request.method

--- hello_world/views.py
+++ hello_world/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world!")
-    
+    # return HttpResponse("Hello, world!")
+    if request.method == "POST":
+       return HttpResponse("You must have POSTed something")
+    elif request.method == "GET":
+       return HttpResponse(request.method)
